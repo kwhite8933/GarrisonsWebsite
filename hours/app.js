b48865a1b1adf7ab92ff6ccf5216d4e76e6aa069
kwhite8933 <kwhite8933@gmail.com>
2016-02-27 14:33:23 -0500

    Contact Us Page Added
    Added a contact us page to the site linked with google maps for easy
    directions.

--- hours/app.js
+++ hours/app.js
@@ -47,41 +47,10 @@ var weeklyHours = [
 $(document).ready( function(){
 
 	var date = new Date();
-	
-	/*var daysOfWeek = [
-		{
-			day: "Sunday",
-			hours: "11 AM - Midnight"
-		},
-		{
-			day: "Monday",
-			hours: "11 AM - Midnight"
-		},
-		{
-			day: "Tuesday",
-			hours: "11 AM - Midnight"
-		},
-		{
-			day: "Wednesday",
-			hours: "11 AM - Midnight"
-		},
-		{
-			day: "Thursday",
-			hours: "11 AM - 1 AM"
-		},
-		{
-			day: "Friday",
-			hours: "11 AM - 1 AM"
-		},
-		{
-			day: "Saturday",
-			hours: "11 AM - 1 AM"
-		}
-	];*/
-
 	var day = date.getDay();
 	var hours = weeklyHours[day].hours;
 
+	console.log(day);
 	$("#todaysHours").append( "<p>Today's Hours:  " + weeklyHours[day].hours + "</p>" );
 
 });
